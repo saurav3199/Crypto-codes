25b1f777f2650c736a0f906aeb10a54f7ce2124b
saurav3199 <saurav.co.in.com@gmail.com>
2019-05-12 13:18:34 +0530

    modified rsa for better execution

---
+++ rsa.py
@@ -0,0 +1,17 @@
+import gmpy2,sys
+import traceback
+import binascii
+
+try:
+    l=map(int,sys.argv[1:])
+    p,n,e=l
+    c=int(input("Enter the ciphertext you want to uncipher:"))
+    q=n//p
+    h=(p-1)*(q-1)
+    d=gmpy2.invert(e,h)
+    plain=hex(pow(c,d,n))[2:]
+    print("The original plaintext is: " + ''.join([chr(int(''.join(c), 16)) for c in zip(plain[0::2],plain[1::2])]))
+    #print(binascii.unhexlify(plain)) # If you want to use binascii
+except Exception:
+    print("Bad Input!!!")
+    #traceback.print_exc()    # If you want to debug
